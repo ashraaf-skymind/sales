f8a771ff013f8ad27b718266ed9dc6f09028c2e7
ashraaf97 <axlrapid97@gmail.com>
2021-04-03 06:33:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 st.title('Technology sales forecasting')
 
-st.write("Here's our first attempt at using data to create a table:")
+st.write("Here's our first attempt at using data to create a table1:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
